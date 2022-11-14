2486d92738de7a961c4234a312883c2d9a9b9000
Konstantin Ta <drpdrp@mail.ru>
2022-11-14 15:02:29 +0300

    ADD: loging

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,6 +2,8 @@ use color_eyre::Result;
 use dotenv::dotenv;
 use eyre::WrapErr;
 use serde::Deserialize;
+use tracing::{info, instrument};
+use tracing_subscriber::EnvFilter;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -10,8 +12,15 @@ pub struct Config {
 }
 
 impl Config {
+    #[instrument]
     pub fn from_env() -> Result<Config> {
         dotenv().ok();
+        tracing_subscriber::fmt()
+            .with_env_filter(EnvFilter::from_default_env())
+            .init();
+
+        info!("Loading configuration");
+
         let mut c = config::Config::new();
         let from_env = config::Environment::default();
         c.merge(from_env)?;
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod config;
 
 use crate::config::Config;
-use actix_web::{App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
 use color_eyre::Result;
 
 #[actix_rt::main]
 async fn main() -> Result<()> {
     let config = Config::from_env().expect("server configuration");
 
-    HttpServer::new(move || App::new())
+    HttpServer::new(move || App::new().wrap(Logger::default()))
         .bind(format!("{}:{}", &config.host, &config.port))?
         .run()
         .await?;
